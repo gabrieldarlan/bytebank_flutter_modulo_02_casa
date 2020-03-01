ec0d9e33075e17408d5938773eec98590e304a0a
gabrieldarlan <gabriel.darlan88@gmail.com>
2020-03-01 20:10:11 -0300

    aula 04 - 03 Carregando a lista de contatos

--- lib/screens/contacts_list.dart
+++ lib/screens/contacts_list.dart
@@ -1,32 +1,26 @@
+import 'package:bytebank_dashboard/models/contact.dart';
 import 'package:bytebank_dashboard/screens/contact_form.dart';
 import 'package:flutter/material.dart';
 
 class ContactsList extends StatelessWidget {
+  final List<Contact> contacts = List();
+
   @override
   Widget build(BuildContext context) {
+    contacts.add(Contact(0,'darlan',2000));
+    contacts.add(Contact(0,'darlan',2000));
+    contacts.add(Contact(0,'darlan',2000));
+    contacts.add(Contact(0,'darlan',1000));
     return Scaffold(
       appBar: AppBar(
         title: Text('Contacts'),
       ),
-      body: ListView(
-        children: <Widget>[
-          Card(
-            child: ListTile(
-              title: Text(
-                'Darlan',
-                style: TextStyle(
-                  fontSize: 24.0,
-                ),
-              ),
-              subtitle: Text(
-                '0000',
-                style: TextStyle(
-                  fontSize: 16.0,
-                ),
-              ),
-            ),
-          )
-        ],
+      body: ListView.builder(
+        itemBuilder: (context, int index) {
+          final Contact contact = contacts[index];
+          return _ContactItem(contact);
+        },
+        itemCount: contacts.length,
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
@@ -37,6 +31,30 @@ class ContactsList extends StatelessWidget {
               .then((newContact) => debugPrint(newContact.toString()));
         },
         child: Icon(Icons.add),
+      ),
+    );
+  }
+}
+
+class _ContactItem extends StatelessWidget {
+  final Contact contact;
+  _ContactItem(this.contact);
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: ListTile(
+        title: Text(
+          contact.name,
+          style: TextStyle(
+            fontSize: 24.0,
+          ),
+        ),
+        subtitle: Text(
+          contact.accountNumber.toString(),
+          style: TextStyle(
+            fontSize: 16.0,
+          ),
+        ),
       ),
     );
   }
